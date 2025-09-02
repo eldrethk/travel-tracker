86afe4f6958d478767cfa29beeda03dee1b37408
Karla Cooper <43483697+eldrethk@users.noreply.github.com>
2025-09-02 13:02:44 -0400

    Restore full app with fixed CosmosClient Registration

--- Program.cs
+++ Program.cs
@@ -27,59 +27,40 @@ builder.Services.AddSingleton(sp =>
     return new BlobServiceClient(builder.Configuration.GetConnectionString("AzureStorage"));
 });
 
+// Always register CosmosClient (this was the fix)
 builder.Services.AddSingleton(sp =>
 {
     var connectionString = builder.Configuration.GetConnectionString("Cosmos");
     return new CosmosClient(connectionString);
 });
 
-// Add Entity Framework - Use In-Memory for free tier, easily switchable to SQL
-if (builder.Environment.IsDevelopment())
-{
-    //builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    // options.UseInMemoryDatabase("TravelExpenseDb"));
-
-}
-else
-{
-    // For production, use SQL Server (comment out for free tier deployment)
-    // builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    //     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-    // For free tier deployment, use In-Memory database
-    builder.Services.AddDbContext<ApplicationDbContext>(options =>
-        options.UseInMemoryDatabase("TravelExpenseDb"));
-}
+// Add Entity Framework for any EF Core needs
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseInMemoryDatabase("TravelExpenseDb"));
 
 builder.Services.AddScoped<ITripService, TripService>();
 builder.Services.AddScoped<IExpenseService, ExpenseService>();
 builder.Services.AddSingleton<IContainerNameResolver, ContainerNameResolver>();
 builder.Services.AddScoped<IBlobService, BlobService>();
-
 builder.Services.AddLogging();
 
 var app = builder.Build();
-
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 app.UseRouting();
-
 app.UseAuthorization();
-
 app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
-
 
 app.Run();
